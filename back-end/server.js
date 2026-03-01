b71d96517bfe8278d71b8789428df2451a8827f8
bank <bankok44man@gmail.com>
2026-03-01 16:21:29 +0700

    fix: increase express json limit to 10mb for base64 image upload

--- back-end/server.js
+++ back-end/server.js
@@ -23,7 +23,7 @@ app.use(cors({
   },
   credentials: true,
 }))
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
 // app.use('/uploads', express.static('uploads'))
 
